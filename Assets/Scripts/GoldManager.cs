694e506ae5402beffe84e09c5ce85539ad952754
Plishh <101051455+Plishh@users.noreply.github.com>
2024-06-29 16:32:41 +0800

    updated gold

---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class GoldManager : MonoBehaviour
+{
+  public int Gold {get; set;}
+
+ [SerializeField] public GameObject goldBox;
+  public TMP_Text goldText;
+
+    private void Start() {
+        Gold = 100;
+        
+        goldText = GetComponent<TMP_Text>();
+        
+        
+    }
+
+    private void Update() {
+        goldText.text = "Gold" + Gold.ToString(); 
+    }
+
+    public void UseGold(int cost) {
+        Gold -= cost;
+    }
+
+    public void AddGold(int reward) {
+        Gold += reward;
+    }
+
+}
